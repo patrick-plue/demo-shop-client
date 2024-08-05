05cc12ad157fc41c9aa7a8808bbc8d44dd1046c2
Patrick Pluequett <ppluequett@gmail.com>
2024-08-05 11:49:25 +0200

    add tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode></React.StrictMode>
+    <React.StrictMode>
+        <div>
+            <p className='text-3xl bold underline'>Hello World!</p>
+        </div>
+    </React.StrictMode>
 );
